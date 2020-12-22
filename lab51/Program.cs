b00dba9ee13dc2378fdc5bd652b4af4ea36aab0e
Victoris <52378018+sviridchik@users.noreply.github.com>
2020-12-22 15:56:21 +0300

    Add files via upload

---
+++ lab51/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using System.Threading;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using FileManager;
+using ServiceLayer;
+
+namespace lab41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var logger = new Logger();
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            AccessAndConnection serviceLayer = new AccessAndConnection(cancellationTokenSource);
+
+            Thread loggerThread = new Thread(new ThreadStart(logger.Start));
+            loggerThread.Start();
+
+            serviceLayer.StartAccessAsync(cancellationTokenSource.Token);
+        }
+    }
+}
